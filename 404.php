112f8c3dd4a2626850a4cbbe15d8d639d93ce403
dayvidwhy <dayvidwhy@gmail.com>
2017-01-27 19:31:37 +1000

    Add 404, htaccess

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'snippets/head.php';?>
+</head>
+<body>
+    <?php include 'snippets/header.php';?>
+    <div role="banner" class="row brown-bg">
+        <div class="grid-container">
+            <h6>404</h6>
+        </div>
+    </div>
+    <main role="main" class="grid-container">
+        <h2 style="margin-top: 16px;">Cat not found</h2>
+        <p>There's nothing here.</p>
+        <img style="margin-bottom: 16px;" src="https://http.cat/404">
+    </main>
+    <?php include 'snippets/footer.php';?>
+</body>
+</html>
